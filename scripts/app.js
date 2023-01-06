ec85f3afdae6dc9e2a8cc6e6b6eba9ff963b3789
Nicolás Vega <nivemo035@hotmail.com>
2023-01-05 22:16:03 -0500

    coreccion se loader

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,8 @@
 
 window.onload = function () {
   setTimeout(function () {
-    var animations = document.getElementsByClassName("loader");
-    for (var i = 0; i < animations.length; i++) {
+    let animations = document.getElementsByClassName("loader");
+    for (let i = 0; i < animations.length; i++) {
       animations[i].style.display = "none";
     }
   }, 1000);
