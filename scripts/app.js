16a46f5d1d26adb4452b6aca57c54bc6049a3939
Nicolás Vega <nivemo035@hotmail.com>
2023-01-17 18:48:12 -0500

    se agrega funcionalidad a botones de encriptar y desencriptar, tambien ahora el textarea right es el que siempre mostrará el resultado

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-/* El loader solo se mostrará 1 segundo luego desaparecerá */
+/* Loader, muestra una carga 1 segundo */
 
 window.onload = function () {
   setTimeout(function () {
@@ -8,3 +8,56 @@ window.onload = function () {
     }
   }, 1000);
 };
+
+/* Funcionalidad de botón y textarea  */
+
+/* Right */
+const textareaRight = document.querySelector(".right__section--textarea");
+const alertTitle = document.querySelector(".right__alert--title");
+const alertText = document.querySelector(".right__alert--text");
+const copyBtn = document.querySelector(".right__btn");
+
+/* Left */
+const textareaLeft = document.querySelector(".main__section__textarea2");
+const sendButton = document.querySelector(".btn-encriptar");
+const encriptar = document.querySelector(".btn-encriptar");
+const desEncriptar = document.querySelector(".btn-desencriptar");
+
+/* Limpia el textarea Right */
+
+document.addEventListener("DOMContentLoaded", function () {
+  textareaRight.value = "";
+  textareaLeft.value = "";
+});
+
+let showAlert = true;
+
+function checkRightLeft() {
+  if (textareaRight.value !== "" && textareaLeft.value.length === 0 && showAlert) {
+    alertTitle.classList.toggle("right__alert--show");
+    alertText.classList.toggle("right__alert--show");
+    showAlert = false;
+  } else if (textareaRight.value === "" || textareaLeft.value.length !== 0) {
+    alertTitle.classList.remove("right__alert--show");
+    alertText.classList.remove("right__alert--show");
+    showAlert = true;
+  }
+}
+
+setInterval(checkRightLeft, 100);
+
+/* funcionalidad encriptar */
+
+encriptar.addEventListener("click", () => {
+  textareaRight.value = textareaLeft.value;
+  textareaLeft.value = "";
+  showText();
+});
+
+/* funcionalidad desencriptar */
+
+desEncriptar.addEventListener("click", () => {
+  textareaRight.value = textareaLeft.value;
+  textareaLeft.value = "";
+  showText();
+});
